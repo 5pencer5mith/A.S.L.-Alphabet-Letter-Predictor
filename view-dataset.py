de399441ec4d0317a5e5ff34f4548535e2b5980d
thomasm1248 <thomasm1248@gmail.com>
2024-04-13 15:55:01 -0600

    Make a tool for printing images for a whole dataset

--- view-dataset.py
+++ view-dataset.py
@@ -3,15 +3,53 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load the dataset
-fileName = sys.argv[1]
-data = util.loadFromFile(fileName)
-data = np.array(data['samples'])
+# List of letters
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# Reshape the data
-size = len(data) * len(data[0])
-data = data.reshape((size, 2))
+# Which points to connect
+connections = [
+    [0, 1],
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [5, 6],
+    [6, 7],
+    [7, 8],
+    [9, 10],
+    [10, 11],
+    [11, 12],
+    [13, 14],
+    [14, 15],
+    [15, 16],
+    [17, 18],
+    [18, 19],
+    [19, 20],
+    [0, 17],
+    [17, 13],
+    [13, 9],
+    [9, 5],
+    [5, 2],
+    [1, 5],
+]
 
-# Plot the data
-plt.scatter(data[:,0], 1-data[:,1])
-plt.show()
+# Get command line args
+name = sys.argv[1]
+whichSampleToDraw = int(sys.argv[2])
+
+# Save one image per letter
+for letter in alphabet:
+    # Get a sample for this letter
+    filename = f"datasets/dataset-{letter}-{name}.pickle"
+    data = util.loadFromFile(filename)
+    data = np.array([data['samples'][whichSampleToDraw]])
+
+    # Create a plot for this letter
+    plt.cla()
+    hand = data[0]
+    lineSegments = [[hand[line[0]], hand[line[1]]] for line in connections]
+    lineSegments = np.array(lineSegments).reshape(-1, 2)
+    lineSegments = np.array([[point[0], 1-point[1]] for point in lineSegments])
+    lineSegments = lineSegments.reshape(-1, 2, 2).swapaxes(1, 2).reshape(-1, 2)
+    plt.plot(*lineSegments, c="k", marker="o")
+
+    plt.savefig(f"dataset-images/{letter}.png")
